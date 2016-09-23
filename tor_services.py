1fffd5355f259e23ff7326e4df85dccdbff5037f
r0oth3x49 <r0oth3x49@gmail.com>
2016-09-23 11:10:24 +0500

    python file to establish circuit with Tor network

--- tor_services.py
+++ tor_services.py
@@ -1,3 +1,11 @@
-import proxy
-p = proxy.Proxy()
-p.ConfigureTor
+#!/usr/bin/python
+
+#######################################################
+#       Tor proxy/network circuit builder             #
+#       Author: Nasir khan (r0ot h3x49)               #
+#######################################################
+#######################################################
+import proxy                                          #
+p = proxy.Proxy()                                     #
+p.ConfigureTor                                        #
+#######################################################
